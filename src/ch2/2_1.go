c7c009de3865f85a21ead6306605ea9a17ade1f6
lcylichenyi <289172988@qq.com>
2020-01-09 13:23:20 +0800

    aa

---
+++ src/ch2/2_1.go
@@ -0,0 +1,29 @@
+package main
+import (
+	"fmt"
+)
+
+type Celsius float64
+type Fahrenheit float64
+type Kelvin float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC 		Celsius = 0
+)
+
+func (c Celsius) String() string { return fmt.Sprintf("%g℃", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%g℉", f) }
+func (k Kelvin) String() string { return fmt.Sprintf("%gK", k) }
+
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+func FToC(f Fahrenheit) Celsius { return Celsius((f-32) * 5 / 9 )}
+
+func CToK(c Celsius) Kelvin { return Kelvin(c + AbsoluteZeroC)}
+
+func KToC(k Kelvin) Celsius { return Celsius(k - Kelvin(AbsoluteZeroC))}
+
+func main() {
+	fmt.Println(CToK(FreezingC))
+}
